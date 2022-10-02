58df615ec70316d7d5220d588662f51b395c1ea1
Jules Bertholet <julesbertholet@quoi.xyz>
2022-10-02 17:50:00 -0400

    Add remove

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,7 @@ unsafe impl<T: Sync> Sync for UnsizedVec<T> {}
 
 impl<T: ?Sized + Aligned> UnsizedVec<T> {
     #[must_use]
+    /// Make a new, empty `UnsizedVec`
     pub fn new() -> Self {
         UnsizedVec {
             ptr: <T as Aligned>::dangling_thin(),
@@ -64,6 +65,7 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
         }
     }
 
+    /// Grow this `UnsizedVec`'s allocation by at least `by_at_least` bytes.
     fn grow(&mut self, by_at_least: usize) {
         // since we set the capacity to usize::MAX when T has size 0,
         // getting to here necessarily means the UnsizedVec is overfull.
@@ -93,21 +95,28 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
         self.cap = new_cap;
     }
 
+    /// Get the offset of one byte past the end of the last element in th vec.
     #[inline]
     fn offset_next(&self) -> usize {
         self.metadata.last().map_or(0, |m| m.offset_next)
     }
 
+    /// Add element to end of the vec.
     pub fn push(&mut self, elem: T) {
         let size_of_val = mem::size_of_val(&elem);
         let offset_next = self.offset_next();
         let new_end_offset = size_of_val + self.offset_next();
+
+        // Make sure we have enough memory
+        self.metadata.reserve(1);
         if new_end_offset >= self.cap {
             self.grow(size_of_val);
         }
 
         let metadata = ptr::metadata(&elem);
 
+        // Copy element to end of allocation.
+        // We checked that we had enough memory earlier
         unsafe {
             ptr::copy_nonoverlapping(
                 core::ptr::addr_of!(elem).cast(),
@@ -116,6 +125,7 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
             );
         }
 
+        // elem is owned by the vec now, don't want to double-drop it
         mem::forget_unsized(elem);
 
         self.metadata.push(MetadataStorage {
@@ -124,6 +134,75 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
         });
     }
 
+    /// Get the offset of one byte past the end of element index - 1 in the vec.
+    #[inline]
+    fn offset_start_idx(&self, index: usize) -> usize {
+        index
+            .checked_sub(1)
+            .map_or(0, |i| self.metadata[i].offset_next)
+    }
+
+    /// Insert element into vec at given index, shifting following elements to the right.
+    pub fn insert(&mut self, index: usize, elem: T) {
+        let size_of_val = mem::size_of_val(&elem);
+        let old_end_offset = self.offset_next();
+        let new_end_offset = size_of_val + old_end_offset;
+
+        // Make sure we have enough memory
+        self.metadata.reserve(1);
+        if new_end_offset >= self.cap {
+            self.grow(size_of_val);
+        }
+
+        let metadata = ptr::metadata(&elem);
+
+        // Update offsets of follwing elements
+        for ms in &mut self.metadata[index..] {
+            ms.offset_next += size_of_val;
+        }
+
+        let offset_start = self.offset_start_idx(index);
+
+        // Shift roght and copy element to end of allocation.
+        // We checked that we had enough memory earlier
+        unsafe {
+            ptr::copy(
+                self.ptr.as_ptr().cast::<u8>().add(offset_start),
+                self.ptr
+                    .as_ptr()
+                    .cast::<u8>()
+                    .add(offset_start)
+                    .add(size_of_val),
+                old_end_offset - offset_start,
+            );
+
+            ptr::copy_nonoverlapping(
+                core::ptr::addr_of!(elem).cast(),
+                self.ptr.as_ptr().cast::<u8>().add(offset_start),
+                size_of_val,
+            );
+        }
+
+        // elem is owned by the vec now, don't want to double-drop it
+        mem::forget_unsized(elem);
+
+        self.metadata.insert(
+            index,
+            MetadataStorage {
+                metadata,
+                offset_next: offset_start + size_of_val,
+            },
+        );
+    }
+
+    // Remove an element from the end of the vec.
+    // This returns the element, which is unsized, through the "emplacer" mechanism.
+    // Call `box_new_with(|e| your_unsized_vec.pop_unwrap(e))` to get the element boxed.
+    //
+    // # Panics
+    //
+    // Panics if len is 0; unfortunately it's not possible to have unsized values inside enums
+    // in Rust, so we can't return `Option`.
     pub fn pop_unwrap(
         &mut self,
         emplacer: &mut dyn FnMut(Layout, <T as Pointee>::Metadata, &mut dyn FnMut(*mut ())),
@@ -133,16 +212,64 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
             offset_next: offset_end,
         } = self.metadata.pop().unwrap();
         let offset_start = self.offset_next();
-        let len = offset_end - offset_start;
+        let size_of_val = offset_end - offset_start;
         emplacer(
-            Layout::from_size_align(len, T::ALIGN).unwrap(),
+            Layout::from_size_align(size_of_val, T::ALIGN).unwrap(),
             metadata,
             &mut |dst| unsafe {
                 ptr::copy_nonoverlapping(
                     self.ptr.as_ptr().cast::<u8>().add(offset_start),
                     dst.cast::<u8>(),
-                    len,
-                )
+                    size_of_val,
+                );
+            },
+        )
+    }
+
+    // Remove the element at the given index, shifting those after it to the left.
+    // This returns the element, which is unsized, through the "emplacer" mechanism.
+    // Call `box_new_with(|e| your_unsized_vec.remove(index, e))` to get the element boxed.
+    //
+    // # Panics
+    //
+    // Panics if index is out of bounds.
+    pub fn remove(
+        &mut self,
+        index: usize,
+        emplacer: &mut dyn FnMut(Layout, <T as Pointee>::Metadata, &mut dyn FnMut(*mut ())),
+    ) {
+        let MetadataStorage {
+            metadata,
+            offset_next: offset_end,
+        } = self.metadata.remove(index);
+        let offset_start = self.offset_start_idx(index);
+
+        let size_of_val = offset_end - offset_start;
+
+        // Update offsets of follwing elements
+        for ms in &mut self.metadata[index..] {
+            ms.offset_next -= size_of_val;
+        }
+
+        emplacer(
+            Layout::from_size_align(size_of_val, T::ALIGN).unwrap(),
+            metadata,
+            &mut |dst| unsafe {
+                ptr::copy_nonoverlapping(
+                    self.ptr.as_ptr().cast::<u8>().add(offset_start),
+                    dst.cast::<u8>(),
+                    size_of_val,
+                );
+
+                ptr::copy(
+                    self.ptr
+                        .as_ptr()
+                        .cast::<u8>()
+                        .add(offset_start)
+                        .add(size_of_val),
+                    dst.cast::<u8>().add(offset_start),
+                    self.offset_next() - offset_start,
+                );
             },
         )
     }
@@ -163,11 +290,7 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
     #[inline]
     fn index_raw(&self, index: usize) -> (*mut (), <T as Pointee>::Metadata) {
         let metadata = self.metadata[index].metadata;
-        let offset = if index == 0 {
-            0
-        } else {
-            self.metadata[index - 1].offset_next
-        };
+        let offset = self.offset_start_idx(index);
         let start_ptr: *mut u8 = self.ptr.as_ptr().cast();
         let offset_ptr = unsafe { start_ptr.add(offset).cast::<()>() };
         (offset_ptr, metadata)
@@ -176,14 +299,16 @@ impl<T: ?Sized + Aligned> UnsizedVec<T> {
 
 impl<T: ?Sized + Aligned> Drop for UnsizedVec<T> {
     fn drop(&mut self) {
-        while let Some(MetadataStorage { metadata, .. }) = self.metadata.pop() {
-            let offset = self.offset_next();
+        if mem::needs_drop::<T>() {
+            while let Some(MetadataStorage { metadata, .. }) = self.metadata.pop() {
+                let offset = self.offset_next();
 
-            let start_ptr: *mut u8 = self.ptr.as_ptr().cast();
-            let offset_ptr = unsafe { start_ptr.add(offset).cast::<()>() };
+                let start_ptr: *mut u8 = self.ptr.as_ptr().cast();
+                let offset_ptr = unsafe { start_ptr.add(offset).cast::<()>() };
 
-            let raw_wide_ptr: *mut T = ptr::from_raw_parts_mut(offset_ptr, metadata);
-            unsafe { raw_wide_ptr.drop_in_place() };
+                let raw_wide_ptr: *mut T = ptr::from_raw_parts_mut(offset_ptr, metadata);
+                unsafe { raw_wide_ptr.drop_in_place() };
+            }
         }
 
         if self.cap != 0 {
@@ -229,6 +354,7 @@ pub fn box_new_with<T: ?Sized>(
     unsized_ret: impl FnOnce(&mut dyn FnMut(Layout, <T as Pointee>::Metadata, &mut dyn FnMut(*mut ()))),
 ) -> Box<T> {
     let mut uninit_box = MaybeUninit::uninit();
+    // FIXME don't allow transmute between types with same metadagta
     unsized_ret(&mut |layout, meta, closure| {
         let box_ptr = unsafe { alloc::alloc(layout) as *mut () };
         closure(box_ptr);
--- tests/test.rs
+++ tests/test.rs
@@ -18,17 +18,32 @@ fn test() {
 fn test_unsized() {
     let mut vec: UnsizedVec<[i32]> = UnsizedVec::new();
     assert_eq!(vec.len(), 0);
+
     let slice: Box<[i32]> = Box::new([1, 2]);
     vec.push(*slice);
     assert_eq!(vec.len(), 1);
     assert_eq!(&vec[0], &[1, 2]);
+
     let slice: Box<[i32]> = Box::new([]);
     vec.push(*slice);
     assert_eq!(&vec[1], &[]);
+
     let slice: Box<[i32]> = Box::new([4, 7, 3]);
     vec.push(*slice);
     vec[2][1] = 19;
     assert_eq!(&vec[2], &[4, 19, 3]);
+
     let popped: Box<[i32]> = box_new_with(|e| vec.pop_unwrap(e));
     assert_eq!(&*popped, &[4, 19, 3]);
+
+    let slice: Box<[i32]> = Box::new([4, 7, 3, 4, 5, 6, 6, -1]);
+    vec.insert(0, *slice);
+    assert_eq!(&vec[0], &[4, 7, 3, 4, 5, 6, 6, -1]);
+    assert_eq!(&vec[1], &[1, 2]);
+    assert_eq!(&vec[2], &[]);
+
+    let removed: Box<[u32]> = box_new_with(|e| vec.remove(1, e));
+    assert_eq!(&*removed, &[1, 2]);
+    assert_eq!(&vec[0], &[4, 7, 3, 4, 5, 6, 6, -1]);
+    assert_eq!(&vec[1], &[]);
 }
