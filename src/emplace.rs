521ed2034cb576f50be3411172f4f132a8b1bc96
Jules Bertholet <julesbertholet@quoi.xyz>
2022-10-07 00:47:53 -0400

    Ensure `box_new_with` doesn't leak memory on unwind

--- src/emplace.rs
+++ src/emplace.rs
@@ -31,12 +31,33 @@ impl<T: ?Sized> Emplacer<T> {
     /// # Safety
     ///
     /// If you unwrap this `Emplacer` and call the resulting closure, you must ensure
-    /// that the closue you pass in writes a valid value of type `T` to the passed-in pointer.
+    /// that the closue you pass in writes a valid value of type `T` to the passed-in pointer
+    /// (or panics, or runs forever, or exits without returning in some other otherwise-sound way).
     pub unsafe fn into_inner(
         &mut self,
     ) -> &mut dyn FnMut(Layout, <T as Pointee>::Metadata, &mut dyn FnMut(*mut PhantomData<T>)) {
         // Safety: `repr(transparent)` guarantees compatible layouts
         unsafe { mem::transmute(self) }
+    }
+}
+
+/// Helper to ensure `box_new_with` doesn't leak memory
+/// if the unsized-value-returning function panics.
+///
+/// Deallocates the contained pointer when dropped.
+/// Must be costructed with a pointer returned by
+/// `alloc::alloc`, and `layout` must be the same
+/// as was passed to the `alloc` call.
+/// Must `mem::forget` the struct to avoid deallocating
+/// its memory.
+struct PointerDeallocer {
+    ptr: *mut u8,
+    layout: Layout,
+}
+
+impl Drop for PointerDeallocer {
+    fn drop(&mut self) {
+        unsafe { alloc::dealloc(self.ptr, self.layout) }
     }
 }
 
@@ -46,9 +67,22 @@ pub fn box_new_with<T: ?Sized>(unsized_ret: impl FnOnce(&mut Emplacer<T>)) -> Bo
     let mut initialized: bool = false;
 
     let closure = &mut |layout, meta, closure: &mut dyn FnMut(*mut PhantomData<T>)| {
-        let box_ptr = unsafe { alloc::alloc(layout) as *mut PhantomData<T> };
-        closure(box_ptr);
-        let init_box = unsafe { Box::from_raw(ptr::from_raw_parts_mut(box_ptr as *mut (), meta)) };
+        // If `closure` panics and triggers an unwind,
+        // this will be dropped, which will call `dealloc` on `ptr`
+        // and ensure no memory is leaked.
+        let deallocer = PointerDeallocer {
+            ptr: unsafe { alloc::alloc(layout) },
+            layout,
+        };
+
+        closure(deallocer.ptr as *mut PhantomData<T>);
+        let init_box =
+            unsafe { Box::from_raw(ptr::from_raw_parts_mut(deallocer.ptr as *mut (), meta)) };
+
+        // Now that we've succesfully initialized the box,
+        // we don't want to deallocate its memory.
+        mem::forget(deallocer);
+
         uninit_box.write(init_box);
 
         initialized = true;
